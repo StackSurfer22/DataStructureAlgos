aeb74ccfa7c00d7c3d25d73ff53ac31a19c1542d
Alex Moreno <amoreno_tech@outlook.com>
2024-11-13 23:34:45 -0600

    added the sum of odd numbers algo

--- javascript/1-myBasic13Algos.js
+++ javascript/1-myBasic13Algos.js
@@ -1,50 +1,67 @@
-//// Setting and Swapping////
-////
+/**
+Basic 13 Algos for warm up and refresher
+some of these algorithms are made without using any premade functions
+for better understanding of the logic
+***/
+
+
+
+/* Setting and Swapping */
 //// “Set myNumber to 42. Set myName to your name. Now swap myNumber into myName & vice versa.”
 
-/////// function setAndSwap(name,number){
-///////     console.log(`Name: ${name} ` +"& " + `Number: ${number}`)
-///////     var myName= number;
-///////     var myNumber = name;
-///////     console.log("Swapped " + `Name: ${myName} ` + `Number: ${myNumber}`);
-/////// }
-///////setAndSwap("Alex",45); //// <----- Call the Function
+function setAndSwap(name,number){
+    console.log(`Name: ${name} ` +"& " + `Number: ${number}`)
+    var myName= number;
+    var myNumber = name;
+    console.log("Swapped " + `Name: ${myName} ` + `Number: ${myNumber}`);
+}
+// setAndSwap("Alex",45); 
 
 
-////// Printing -52 to 1006
-///
-//////“Print integers from -52 to 1066 using a FOR loop.”
 
-/////// function printingNums(){
-///////     for(var i = -52 ; i<1067 ; i++){
-///////         console.log(i);
-///////     }
-/////// }
-/////// printingNums() //// <----- Call the Function
+////// Print integers from -1 to 50 using a FOR loop.”
+function printingNums(){
+    for(var i = -1 ; i<51 ; i++){
+        console.log("printing number"+i);
+    }
+}
+// printingNums()
 
 
-////// Don't worry be happy
-/////////
-///“Create - beCheerful() - Within it, console.log string "good morning!" Call it 98 times.”
 
-////// function beCheerful(){
-//////     for(var i=0; i <98 ; i++){
-//////         console.log("Good Morning")
-//////     }
-////// }
-////// beCheerful() //// <----- Call the Function
+//// “Create - goodMorning() - Within it, console.log string "good morning!" Call it 10 times.”
+
+function goodMorning(){
+    for(var i=0; i <11 ; i++){ // the computer starts counting from 0 so we need to add 1 to the number.
+        console.log("Good Morning" + i);
+    }
+}
+goodMorning() //// <----- Call the Function
 
 
-////“Multiples of Three – but Not All
-//////
-///// Using FOR, print multiples of 3 from -300 to 0. Skip -3 and -6.”
+
+///// Using FOR loop, print multiples of 3 from -30 to 0. Skip -3 and -6.”
 
 function multiplesOf3(){
-    for(var i = -300 ; i < 0 ; i++){
+    for(var i = -30 ; i < 0 ; i++){
         if(i == -3 && i == -6){
-            console.log("theres a 3 or a 6");
+            console.log("theres a -3 or -6");
         }
         else{console.log(i * 3)};
     }
 }
-multiplesOf3();
+// multiplesOf3();
+
+
+
+//// SUMMING odd number to 1000
+function sumOdd(){
+    let sum = 0;
+    for(let i = 0; i <= 1000; i++){
+        if(i % 2 == i){
+            sum += i;
+        }
+    }
+    return sum;
+}
+sumOdd();
