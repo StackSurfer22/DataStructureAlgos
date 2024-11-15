ec3611e3a91a1a6cf89e4f1e1b574cb211dfe927
Alex Moreno <amoreno_tech@outlook.com>
2024-11-15 00:04:08 -0600

    daily algos added | shiftArrayLeft,replaceNegatives,printMaxNum

--- javascript/1-myBasic13Algos.js
+++ javascript/1-myBasic13Algos.js
@@ -1,13 +1,12 @@
-/**
-Basic 13 Algos for warm up and refresher
-some of these algorithms are made without using any premade functions
-for better understanding of the logic
-***/
+// /**
+// Basic 13 Algos for warm up and refresher
+// some of these algorithms are made without using any premade functions
+// for better understanding of the logic
+// ***/
 
 
 
-/* Setting and Swapping */
-//// “Set myNumber to 42. Set myName to your name. Now swap myNumber into myName & vice versa.”
+ /**** Setting and Swapping my name and a number ****/
 
 function setAndSwap(name,number){
     console.log(`Name: ${name} ` +"& " + `Number: ${number}`)
@@ -15,53 +14,97 @@ function setAndSwap(name,number){
     var myNumber = name;
     console.log("Swapped " + `Name: ${myName} ` + `Number: ${myNumber}`);
 }
-// setAndSwap("Alex",45); 
+setAndSwap("Alex",45);
+setAndSwap("Mike", 23);
 
 
-
-////// Print integers from -1 to 50 using a FOR loop.”
-function printingNums(){
-    for(var i = -1 ; i<51 ; i++){
-        console.log("printing number"+i);
+/****  Print integers from -1 to 10 using a FOR loop.” ****/
+for(var i = -1 ; i<10 ; i++){
+    function printingNums(){
+        console.log("printing number "+i);
     }
 }
-// printingNums()
+printingNums()
 
 
 
-//// “Create - goodMorning() - Within it, console.log string "good morning!" Call it 10 times.”
+// /****  create a function that says good morning 10 times ****/
 
 function goodMorning(){
     for(var i=0; i <11 ; i++){ // the computer starts counting from 0 so we need to add 1 to the number.
-        console.log("Good Morning" + i);
+        console.log("Good Morning " + i);
     }
 }
-goodMorning() //// <----- Call the Function
+goodMorning();
 
 
 
-///// Using FOR loop, print multiples of 3 from -30 to 0. Skip -3 and -6.”
+/**** Using FOR loop, print multiples of 3 from -10 to 0. Skip -3 and -6. ****/
 
 function multiplesOf3(){
-    for(var i = -30 ; i < 0 ; i++){
+    for(var i = -10 ; i < 0 ; i++){
         if(i == -3 && i == -6){
             console.log("theres a -3 or -6");
         }
         else{console.log(i * 3)};
     }
 }
-// multiplesOf3();
+multiplesOf3();
 
-
-
-//// SUMMING odd number to 1000
+/**** Return the sum of odd numbers in a array ****/
 function sumOdd(){
     let sum = 0;
-    for(let i = 0; i <= 1000; i++){
-        if(i % 2 == i){
+    for(let i = 0; i <= 100; i++){
+        if(i % 2 == 1){
             sum += i;
         }
     }
-    return sum;
+    console.log("Sum of odd number is: " + sum);
 }
 sumOdd();
+
+/**** Shifting array values left ****/
+
+function shiftArray(arr){
+    temp = arr[0];
+    for(let i = 1; i < arr.length; i++){
+        arr[i-1] = arr[i];
+    }
+    arr[arr.length-1] = temp;
+    console.log(arr);
+}
+shiftArray([1,2,3,4,5,6,7,8,9,10]);
+
+
+
+/**** Replacing negative numbers with the string "negNum" ****/
+function replaceNegatives(arr){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] < 0){
+            arr[i] = "negNum";
+        };
+    }
+    console.log(arr);
+}
+replaceNegatives([-1,0,-3,4,-5,6,-7,8,-9,10]);
+
+
+
+
+/**** Print the max number of an array    ****/
+function printMaxNum(arr){
+    if(arr.length == 0 || null){
+        console.log("Array is empty");
+        return;
+    }
+
+    let max = arr[0];
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    console.log("Max number is: " + max);
+}
+printMaxNum([])
+printMaxNum([1,2,3,4,5,6,7,8,9,10]);
